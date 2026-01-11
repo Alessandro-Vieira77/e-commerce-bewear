6964649f7a320940e6ac82775127df2b756d72ad
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2026-01-11 15:02:49 -0300

    feat: add category products page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -4,25 +4,28 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/src/db/schema";
 import { formatInCentToBRL } from "@/src/helpers/money";
+import { cn } from "@/src/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  className?: string;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+export function ProductItem({ product, className }: ProductItemProps) {
   const firstVariant = product?.variants[0];
   return (
     <Link href="/" className="flex flex-col gap-4">
       <Image
         src={firstVariant?.imageUrl}
         alt={product?.name}
-        width={200}
-        height={200}
-        className="rounded-3xl"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div className={cn("flex max-w-[200px] flex-col gap-1", className)}>
         <p className="truncate text-sm font-medium">{product?.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product?.description}

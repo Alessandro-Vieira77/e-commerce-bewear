5e26c62b1e5ba57b0316c15655acbfb08307bbab
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2026-01-30 12:55:58 -0300

    feat: add navigation for menu

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ShoppingBasketIcon } from "lucide-react";
+import { ShoppingBag, ShoppingBasketIcon } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/src/components/ui/button";
@@ -18,71 +18,84 @@ import { useCart } from "@/src/hooks/queries/use-cart";
 
 import CartItem from "./cart-item";
 
-export const Cart = () => {
+export const Cart = ({ menuOpen }: { menuOpen: string }) => {
   const { data: cart } = useCart();
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline" size="icon">
-          <ShoppingBasketIcon />
-        </Button>
+        {menuOpen === "menu" ? (
+          <Button variant="outline" size="icon">
+            <ShoppingBasketIcon />
+          </Button>
+        ) : (
+          <div className="flex gap-2">
+            <ShoppingBag size={16} />{" "}
+            <p className="text-xs font-medium">Sacola</p>
+          </div>
+        )}
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
 
-        <div className="flex h-full flex-col px-5 pb-5">
-          <div className="flex h-full max-h-full flex-col overflow-hidden">
-            <ScrollArea className="h-full">
-              <div className="flex h-full flex-col gap-8">
-                {cart?.items.map((item) => (
-                  <CartItem
-                    key={item.id}
-                    id={item.id}
-                    productName={item.productVariant.product.name}
-                    productVariantName={item.productVariant.name}
-                    productVariantImageUrl={item.productVariant.imageUrl}
-                    productVariantPriceInCents={
-                      item.productVariant.priceInCents
-                    }
-                    productVariantId={item.productVariant.id}
-                    quantity={item.quantity}
-                  />
-                ))}
-              </div>
-            </ScrollArea>
-          </div>
-
-          {cart?.items && cart?.items.length > 0 && (
-            <div className="flex flex-col gap-4">
-              <Separator />
-
-              <div className="flex items-center justify-between text-xs font-medium">
-                <p>Subtotal</p>
-                <p>{formatInCentToBRL(cart?.totalPriceInCents ?? 0)}</p>
-              </div>
-
-              <Separator />
-
-              <div className="flex items-center justify-between text-xs font-medium">
-                <p>Entrega</p>
-                <p>GRÁTIS</p>
-              </div>
-
-              <Separator />
-
-              <div className="flex items-center justify-between text-xs font-medium">
-                <p>Total</p>
-                <p>{formatInCentToBRL(cart?.totalPriceInCents ?? 0)}</p>
-              </div>
-
-              <Button className="mt-5 rounded-full">
-                <Link href="/cart/identification">Finalizar compra</Link>
-              </Button>
+        {cart?.items && cart?.items.length > 0 ? (
+          <div className="flex h-full flex-col px-5 pb-5">
+            <div className="flex h-full max-h-full flex-col overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="flex h-full flex-col gap-8">
+                  {cart?.items.map((item) => (
+                    <CartItem
+                      key={item.id}
+                      id={item.id}
+                      productName={item.productVariant.product.name}
+                      productVariantName={item.productVariant.name}
+                      productVariantImageUrl={item.productVariant.imageUrl}
+                      productVariantPriceInCents={
+                        item.productVariant.priceInCents
+                      }
+                      productVariantId={item.productVariant.id}
+                      quantity={item.quantity}
+                    />
+                  ))}
+                </div>
+              </ScrollArea>
             </div>
-          )}
-        </div>
+
+            {cart?.items && cart?.items.length > 0 && (
+              <div className="flex flex-col gap-4">
+                <Separator />
+
+                <div className="flex items-center justify-between text-xs font-medium">
+                  <p>Subtotal</p>
+                  <p>{formatInCentToBRL(cart?.totalPriceInCents ?? 0)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between text-xs font-medium">
+                  <p>Entrega</p>
+                  <p>GRÁTIS</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between text-xs font-medium">
+                  <p>Total</p>
+                  <p>{formatInCentToBRL(cart?.totalPriceInCents ?? 0)}</p>
+                </div>
+
+                <Button className="mt-5 rounded-full">
+                  <Link href="/cart/identification">Finalizar compra</Link>
+                </Button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="flex h-full flex-col items-center">
+            <p>Sua sacola está vazia</p>
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   );
---
+++ src/components/common/menu.tsx
@@ -0,0 +1,140 @@
+"use client";
+import {
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ShoppingBag,
+  TruckIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { toast } from "sonner";
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/src/components/ui/avatar";
+import { Button } from "@/src/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/src/components/ui/sheet";
+import { authClient } from "@/src/lib/auth-client";
+
+import { Separator } from "../ui/separator";
+import { Cart } from "./cart";
+
+export const Menu = () => {
+  const { data: session } = authClient.useSession();
+
+  const handleLogout = () => {
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          toast("Logout realizado com sucesso");
+        },
+        onError: () => {
+          toast("Erro ao realizar logout");
+        },
+      },
+    });
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle className="font-bold">Menu</SheetTitle>
+        </SheetHeader>
+        <div className="px-5">
+          {session?.user ? (
+            <>
+              <div className="flex justify-between space-y-6">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage
+                      src={session?.user?.image as string | undefined}
+                    />
+                    <AvatarFallback>
+                      {session?.user?.name?.split(" ")?.[0]?.[0]}
+                      {session?.user?.name?.split(" ")?.[1]?.[0]}
+                    </AvatarFallback>
+                  </Avatar>
+
+                  <div>
+                    <h3 className="text-sm font-semibold sm:text-base">
+                      {session?.user?.name}
+                    </h3>
+                    <span className="text-muted-foreground block text-xs sm:text-sm">
+                      {session?.user?.email}
+                    </span>
+                  </div>
+                </div>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => authClient.signOut()}
+                >
+                  <LogOutIcon />
+                </Button>
+              </div>
+            </>
+          ) : (
+            <div className="flex items-center justify-between">
+              <h2 className="font-semibold">Olá. Faça seu login!</h2>
+              <Button size="icon" asChild variant="outline">
+                <Link href="/authentication">
+                  <LogInIcon />
+                </Link>
+              </Button>
+            </div>
+          )}
+        </div>
+
+        <div className="px-5">
+          <Separator />
+          {/* navigation links */}
+          <div className="flex flex-col gap-3 pt-5">
+            <Link href="/">
+              <div className="flex gap-2">
+                <HomeIcon size={16} />{" "}
+                <p className="text-xs font-medium">Início</p>
+              </div>
+            </Link>
+
+            <Link href="/my-orders">
+              <div className="flex gap-2">
+                <TruckIcon size={16} />{" "}
+                <p className="text-xs font-medium">Meus pedidos</p>
+              </div>
+            </Link>
+            <Cart menuOpen="bag" />
+          </div>
+        </div>
+
+        <div className="px-5">
+          <Separator />
+        </div>
+        <div className="w-full px-3">
+          <Button
+            variant="ghost"
+            className="text-muted-foreground font-regular"
+            onClick={handleLogout}
+          >
+            <LogOutIcon size={16} />
+            Logout
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
